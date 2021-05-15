63576510656381f5ba9b3c6bc2da426c23454e31
black1ava <black1ava@github.com>
2021-05-15 15:35:47 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+  const [fetch, setFetch] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function(){
+    setIsLoading(true);
+    axios.get('http://localhost:4000/v1')
+      .then(function(response){
+        setTasks(response.data);
+        setIsLoading(false);
+      })
+      .catch(function(err){
+        console.error(err)
+      });
+  }, [fetch]);
+
+  function handleChange(e){
+    setTask(e.target.value);
+  }
+
+  function onAddTask(e){
+    e.preventDefault();
+    setIsLoading(false);
+    axios.post('http://localhost:4000/v1', { task })
+      .then(function(response){
+        console.log(response, { task })
+      })
+      .then(function(){
+        setFetch(prevFetch => ++prevFetch);
+        setTask('');
+      })
+      .catch(function(err){
+        console.error(err)
+      })
+  }
+
+  function onDelete(id){
+    setIsLoading(false);
+    axios.delete('http://localhost:4000/v1', { data: { id } } )
+      .then(function(response){
+        console.log(response);
+      })
+      .then(function(){
+        setFetch(prevFetch => --prevFetch)
+      })
+      .catch(function(err){
+        console.error(err)
+      })
+  }
+
+  const loading = isLoading && (
+    <div className="loader"></div>
   );
+
+  const tasksList = tasks.map(function(task){
+    return (
+      <li key={ task._id } onClick={ onDelete.bind(this, task._id) }>
+        { task.task }
+      </li>
+    );
+  });
+
+  return(
+    <div className="container">
+      <form onSubmit={ onAddTask }>
+        <input type="text" value={ task } onChange={ handleChange } placeholder="Add a new task!!"/>
+        <button>Add Task</button>
+      </form>
+      <ul>
+        { tasksList }
+      </ul>
+      { loading }
+    </div>
+  )
 }
 
 export default App;
